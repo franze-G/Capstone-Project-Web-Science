73853521f5e41af3a2a73133aa275cd9c69035c7
Franze <131516686+franze-G@users.noreply.github.com>
2024-07-08 12:26:51 +0800

    Logout function

--- app/Models/Freelancer.php
+++ app/Models/Freelancer.php
@@ -7,23 +7,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // use Illuminate\Database\Eloquent\Model;
 
-<<<<<<<< HEAD:app/Models/User.php
-class User extends Authenticatable
-{
-    use HasFactory, Notifiable;
-
-    protected $table = 'freelance_user_table';
-    
-========
 class Freelancer extends Authenticatable
 {
     use HasFactory, Notifiable;
->>>>>>>> Andrei:app/Models/Freelancer.php
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    
     protected $fillable = [
         'firstname',
         'lastname',
