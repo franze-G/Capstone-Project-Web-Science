134971de7d8a2bbfb9bd2c2b1da6a52428ce08ef
Franze <131516686+franze-G@users.noreply.github.com>
2024-07-09 11:20:40 +0800

    Delete

--- resources/views/components/input-label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-black dark:text-gray-300']) }}>
-    {{ $value ?? $slot }}
-</label>
--- resources/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-black border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green dark:hover:bg-green focus:bg-black dark:focus:bg-white active:bg-black dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-black-800 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
