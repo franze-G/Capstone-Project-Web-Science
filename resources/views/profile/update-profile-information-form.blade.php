8be63d7b9aba05dc2d42064b23e8a842576f8d8e
Franze <131516686+franze-G@users.noreply.github.com>
2024-07-13 20:32:19 +0800

    Email verification - Team invite

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
-                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                    <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->firstname }}" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
                 <!-- New Profile Photo Preview -->
@@ -54,9 +54,16 @@
 
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
-            <x-input-error for="name" class="mt-2" />
+            <x-label for="firstname" value="{{ __('Firstname') }}" />
+            <x-input id="firstname" type="text" class="mt-1 block w-full" wire:model="state.firstname" required autocomplete="firstname" />
+            <x-input-error for="firstname" class="mt-2" />
+        </div>
+
+         <!-- Position -->
+         <div class="col-span-6 sm:col-span-4">
+            <x-label for="position" value="{{ __('Position') }}" />
+            <x-input id="position" type="text" class="mt-1 block w-full" wire:model="state.position" required autocomplete="position" />
+            <x-input-error for="position" class="mt-2" />
         </div>
 
         <!-- Email -->
