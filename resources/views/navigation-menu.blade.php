60452152667f1ebe92704da1c2d5a625080ebba8
Franze <131516686+franze-G@users.noreply.github.com>
2024-07-15 10:06:47 +0800

    fix Nav-menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,10 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    
-                    {{-- cinacall nya yung isClient sa model which is mag dedetermine kung yung currently login user is client or isFreelancer na nasa models din. --}}
-                    
+
+                    {{-- cinacall nya yung isClient sa model which is mag dedetermine kung yung currently 
+                    login user is client or isFreelancer na nasa models din. --}}
+
                     @if (Auth::user()->isClient())
                         <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
@@ -25,7 +26,6 @@
                         </x-nav-link>
                     @endif
                 </div>
-                
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -37,7 +37,6 @@
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->firstname }}
-
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                         </svg>
@@ -68,7 +67,9 @@
                                         @endif
 
                                         <!-- Team Switcher -->
-                                        @if (Auth::user()->isClient() && Auth::user()->allTeams()->count() > 1)
+                                        @if (Auth::user()->isClient() && Auth::user()->allTeams()->count() > 0) 
+                                                            {{-- pag yung existing count ng team is more than 0, dapat mag display yung team.  --}}
+
                                             <div class="border-t border-gray-200"></div>
 
                                             <div class="block px-4 py-2 text-xs text-gray-400">
@@ -108,7 +109,6 @@
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->firstname }}
-
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                         </svg>
@@ -118,6 +118,7 @@
                         </x-slot>
 
                         <x-slot name="content">
+
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
                                 {{ __('Manage Account') }}
@@ -164,9 +165,16 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            {{-- Check user type and set navigation link --}}
+            @if (Auth::user()->isClient())
+                <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+            @elseif (Auth::user()->isFreelancer())
+                <x-responsive-nav-link href="{{ route('freelancer.home') }}" :active="request()->routeIs('freelancer.home')">
+                    {{ __('Overview') }}
+                </x-responsive-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
@@ -229,7 +237,7 @@
                     @endif
 
                     <!-- Team Switcher -->
-                    @if (Auth::user()->isClient() && Auth::user()->allTeams()->count() > 1)
+                    @if (Auth::user()->isClient() && Auth::user()->allTeams()->count() > 0)
                         <div class="border-t border-gray-200"></div>
 
                         <div class="block px-4 py-2 text-xs text-gray-400">
