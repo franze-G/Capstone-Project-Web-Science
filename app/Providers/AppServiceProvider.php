bd3cdc3930609ae021c4400697bb908e3c6de27a
Andrei Huyo-a <carl.andrei.del.rosario@adamson.edu.ph>
2024-07-17 12:05:53 +0800

    welcuh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Blade::anonymousComponentNamespace(__DIR__ . '/../client');
-        Blade::anonymousComponentNamespace(__DIR__ . '/../client', 'client');
     }
 }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ComponentServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
